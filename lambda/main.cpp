0198291ffe839369fcb15101962829b5617afd8f
jatomic <johnatomich@gmail.com>
2022-02-17 19:23:08 -0600

    Anonymous, No Captures, Parameters, Arguments, Immediate

--- lambda/main.cpp
+++ lambda/main.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+
+// I CAN EITHER NAME THE FUNCTION...
+// -- -- -- -- -- --
+/*auto andrew_lambda = [](){
+    std::cout << "Hey, Drew!" << std::endl;
+};*/
+
+int main(){
+    // ... AND CALL IT LATER BY NAME
+    // -- -- --
+    //andrew_lambda();
+
+    // OR SIMPLY DEFINE THE FUNCTION ANONYMOUSLY AND CALL IT IMMEDIATELY WITH ()
+    // -- -- -- -- -- --
+    /*[](){
+        std::cout << "Hey, Drew!" << std::endl;
+    }();*/
+
+    double x {2.7}, y {3.9};
+
+    [](double a, double b){
+        std::cout << a + b << std::endl;
+    }(x, y);
+    
+    return 0;
+}
